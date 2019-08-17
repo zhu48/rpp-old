77bf1b5bc765c307d252488f5c5e0523228d9615
Zuodian Hu <huzuodian@gmail.com>
2019-08-17 15:48:54 -0500

    feat(test): add code to add threads to a scheduler to scheduler test

--- test/test_sched.cpp
+++ test/test_sched.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+
 #include "scheduler.hpp"
 
+void print_tid( void* const arg ) {
+    std::cout << "thread " << *static_cast<int*>( arg ) << std::endl;
+}
+
 int main( int argc, char* argv[] ) {
-    return 0;
+    rpp::scheduler<2> cpu0;
+
+    auto thrd_0_id = cpu0.add_thread( rpp::thread( print_tid, nullptr ) );
+    auto thrd_1_id = cpu0.add_thread( rpp::thread( print_tid, nullptr ) );
+
+    cpu0.run();
+
+    return -1;
 }
