bc748dc0deb042e252db76d719c11f47a16d22a2
Zuodian Hu <huzuodian@gmail.com>
2019-08-18 10:51:30 -0500

    feat(build): factor out compiler selection message

--- build/build_cfg.cmake
+++ build/build_cfg.cmake
@@ -7,8 +7,9 @@ cmake_minimum_required(VERSION 3.3)
 
 set(CMAKE_BUILD_TYPE Debug CACHE STRING "" FORCE)
 
-set(RPP_TARGET_PLATFORM "NONE"    CACHE STRING "Target platform." FORCE)
-set(RPP_COMPILER        "DEFAULT" CACHE STRING "Target compiler" FORCE)
+set(RPP_TARGET_PLATFORM "NONE"    CACHE STRING "Target platform"              FORCE)
+set(RPP_COMPILER        "DEFAULT" CACHE STRING "Target compiler"              FORCE)
+set(RPP_TEST_TARGET     "TRUE"    CACHE BOOL   "Flag to create a test target" FORCE)
 
 ##############################
 # Valid Configuration Values #
@@ -27,21 +28,21 @@ set(RPP_COMPILER_OPTS
 # Toolchain Path Configuration #
 ################################
 
+message(STATUS "Configured target device family: ${RPP_TARGET_PLATFORM}")
+message(STATUS "Configured target compiler     : ${RPP_COMPILER}")
+
 if(RPP_TARGET_PLATFORM IN_LIST RPP_TARGET_PLATFORM_OPTS)
     if(RPP_COMPILER STREQUAL "DEFAULT")
-        message(STATUS "Compiling for testing on host platform")
+        message(STATUS "Using default compiler for build environment")
     elseif(RPP_COMPILER IN_LIST RPP_COMPILER_OPTS)
         # set toolchain file based on target architecture.
         set(CMAKE_TOOLCHAIN_FILE "${RPP_TARGET_PLATFORM}_${RPP_COMPILER}.cmake" CACHE PATH "" FORCE)
     else()
-        message(STATUS "Configured target device family: ${RPP_TARGET_PLATFORM}")
-        message(STATUS "Configured target compiler     : ${RPP_COMPILER}")
         message(STATUS "When cross-compiling, set RPP_COMPILER to one of the folowing:")
         message(STATUS "    ${RPP_COMPILER_OPTS}")
         message(FATAL_ERROR "Unsupported cross-compilation toolchain: ${RPP_COMPILER}")
     endif()
 else()
-    message(STATUS "Configured target device family: ${RPP_TARGET_PLATFORM}")
     message(STATUS "Set RPP_TARGET_PLATFORM to one of the following:")
     message(STATUS "    ${RPP_TARGET_PLATFORM_OPTS}")
     message(FATAL_ERROR "Unsupported target device family: ${RPP_TARGET_PLATFORM}.")
