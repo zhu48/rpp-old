c308026a419488e4cfa6f1d8de81c260c6a64ac3
Zuodian Hu <huzuodian@gmail.com>
2019-09-15 14:03:57 -0500

    feat(sched): add private priority queue to scheduler object

--- kernel/impl/src/scheduler.cpp
+++ kernel/impl/src/scheduler.cpp
@@ -2,8 +2,32 @@
 
 using namespace rpp;
 
+bool scheduler::decision_data::operator==( const decision_data& rhs ) const {
+    return this->prio == rhs.prio;
+}
+
+bool scheduler::decision_data::operator!=( const decision_data& rhs ) const {
+    return this->prio != rhs.prio;
+}
+
+bool scheduler::decision_data::operator<( const decision_data& rhs ) const {
+    return this->prio < rhs.prio;
+}
+
+bool scheduler::decision_data::operator>( const decision_data& rhs ) const {
+    return this->prio > rhs.prio;
+}
+
+bool scheduler::decision_data::operator>=( const decision_data& rhs ) const {
+    return this->prio >= rhs.prio;
+}
+
+bool scheduler::decision_data::operator<=( const decision_data& rhs ) const {
+    return this->prio <= rhs.prio;
+}
+
 void scheduler::add_thread( thread& new_thread ) {
-    
+    data.push( decision_data{ &new_thread, 0 } );
 }
 
 [[noreturn]] void scheduler::run( void ) noexcept {
--- kernel/include/scheduler.hpp
+++ kernel/include/scheduler.hpp
@@ -1,8 +1,12 @@
 #ifndef SCHEDULER_HPP
 #define SCHEDULER_HPP
 
+#include <cstdint>
+
+#include <optional>
 #include <queue>
-#include <array>
+
+#include "queue_array.hpp"
 
 #include "thread.hpp"
 
@@ -14,6 +18,27 @@ namespace rpp {
      * \details Generic scheduler for running threads.
      */
     class scheduler {
+        public:
+            using priority_t = std::uint_fast16_t;
+
+            struct decision_data {
+                thread*    thrd = nullptr;
+                priority_t prio = 0;
+
+                bool operator==( const decision_data& rhs ) const;
+                bool operator!=( const decision_data& rhs ) const;
+                bool operator<( const decision_data& rhs ) const;
+                bool operator>( const decision_data& rhs ) const;
+                bool operator>=( const decision_data& rhs ) const;
+                bool operator<=( const decision_data& rhs ) const;
+            };
+
+        private:
+            std::priority_queue<
+                decision_data,
+                queue_array<decision_data,16>
+            > data;
+
         public:
             /**
              * \brief Add a new thread for this scheduler to schedule.
