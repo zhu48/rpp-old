cc3b38238887ed4923b470caf5afa289b0383892
Zuodian Hu <huzuodian@gmail.com>
2019-09-17 16:23:00 -0500

    feat(test): exercise the scheduler with array of threads

--- test/test_sched.cpp
+++ test/test_sched.cpp
@@ -8,7 +8,18 @@ void print_tid( void* const arg ) {
 }
 
 int main( int argc, char* argv[] ) {
-    std::array<rpp::decision_data,4> dec_arr;
+    std::array<rpp::thread,4> thrd_arr = {
+        rpp::thread( print_tid, nullptr ),
+        rpp::thread( print_tid, nullptr ),
+        rpp::thread( print_tid, nullptr ),
+        rpp::thread( print_tid, nullptr )
+    };
+    std::array<rpp::decision_data,4> dec_arr = {
+        rpp::decision_data{ &(thrd_arr[0]), 0 },
+        rpp::decision_data{ &(thrd_arr[1]), 0 },
+        rpp::decision_data{ &(thrd_arr[2]), 0 },
+        rpp::decision_data{ &(thrd_arr[3]), 0 }
+    };
 
     rpp::run(
         rpp::dynamic_span<rpp::decision_data>{
