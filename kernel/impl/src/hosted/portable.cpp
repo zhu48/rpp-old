3b35b5669b667a5ad161ff19ae8eb5e6301ad7fb
Zuodian Hu <huzuodian@gmail.com>
2019-09-18 05:40:15 -0500

    feat(port): create and detach thread on hosted systems when setting up fresh stack

--- kernel/impl/src/hosted/portable.cpp
+++ kernel/impl/src/hosted/portable.cpp
@@ -1,6 +1,16 @@
+#include <thread>
+
 #include "portable.h"
 
 using namespace portable;
+
+namespace {
+
+    void store_handle( context_t* const context, std::thread::native_handle_type handle ) {
+        *reinterpret_cast<std::thread::native_handle_type*>( context ) = handle;
+    }
+
+}
 
 void portable::initialize_stack(
     void* const      stack,
@@ -8,7 +18,9 @@ void portable::initialize_stack(
     entry_fn_arg_t   arg,
     context_t* const context
 ) {
-
+    std::thread thrd( entry, arg );
+    store_handle( context, thrd.native_handle() );
+    thrd.detach();
 }
 
 void portable::switch_context( context_t* const context ) {
