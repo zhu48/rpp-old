d3ee16966aeed639fefe2f62131e0393bf6483c8
Zuodian Hu <huzuodian@gmail.com>
2019-09-19 22:34:05 -0500

    chore(port): remove C compatibility handling from portable types header

--- kernel/include/portable_types.hpp
+++ kernel/include/portable_types.hpp
@@ -1,32 +1,26 @@
 #ifndef PORTABLE_TYPES_H
 #define PORTABLE_TYPES_H
 
-#ifdef __cplusplus
 namespace portable {
-extern "C" {
-#endif // #ifdef __cplusplus
 
-/**
- * \brief Type used as the thread entry point function argument.
- */
-typedef void* const entry_fn_arg_t;
+    /**
+     * \brief Type used as the thread entry point function argument.
+     */
+    typedef void* const entry_fn_arg_t;
 
-/**
- * \brief Function type used as entry point to thread execution.
- */
-typedef void (*entry_fn_t)( entry_fn_arg_t );
+    /**
+     * \brief Function type used as entry point to thread execution.
+     */
+    typedef void (*entry_fn_t)( entry_fn_arg_t );
 
-/**
- * \brief Thread context type.
- */
-typedef struct {
-    const void* stack_base; //!< Pointer to thread's allocated stack's base.
-    const void* frame_base; //!< Pointer to thread's current stack frame.
-} context_t;
+    /**
+     * \brief Thread context type.
+     */
+    typedef struct {
+        const void* stack_base; //!< Pointer to thread's allocated stack's base.
+        const void* frame_base; //!< Pointer to thread's current stack frame.
+    } context_t;
 
-#ifdef __cplusplus
-} // extern "C"
-} // namespace portable
-#endif // #ifdef __cplusplus
+}
 
 #endif // #ifndef PORTABLE_TYPES_H
