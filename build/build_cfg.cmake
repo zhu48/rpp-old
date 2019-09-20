1b2b6db9b102b1f5264c38e044fe1d34304edf96
Zuodian Hu <huzuodian@gmail.com>
2019-09-19 22:55:20 -0500

    feat(build): generate project-specific platform constants header during build environment configuration

--- build/build_cfg.cmake
+++ build/build_cfg.cmake
@@ -51,6 +51,13 @@ else()
     set(RPP_PTHREADS FALSE CACHE BOOL "Requirement to link to pthreads library")
 endif()
 
+# set required platform-dependent constants
+if(RPP_TARGET_PLATFORM STREQUAL "xc7z")
+    set(RPP_CONTEXT_SIZE "128" CACHE STRING "Size of thread context block")
+else()
+    set(RPP_CONTEXT_SIZE "8"   CACHE STRING "Size of thread context block")
+endif()
+
 ########################
 # Vivado Configuration #
 ########################
