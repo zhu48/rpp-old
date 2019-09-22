105b69c9a14670c6d345feb33930c54797c6e97f
Zuodian Hu <huzuodian@gmail.com>
2019-09-22 14:24:50 -0500

    feat(port): reset puts non-primary cores to sleep

--- kernel/impl/src/xc7z/startup.c
+++ kernel/impl/src/xc7z/startup.c
@@ -1,5 +1,15 @@
 void reset( void ) __attribute__ ((naked)) {
-
+    __ASM volatile(
+        // disable all interrupts
+        "    CPSID if                    \n" // disable all IRQ and FIQ interrupts
+        // if this is not core zero, sleep until an interrupt handler makes this core do something
+        "    MRC   p15, 0, R0, c0, c0, 5 \n" // read the Multiprocessor Affinity Register into R0
+        "    ANDS  R0, R0, #3            \n" // mask lowest two bits, which contain the core ID
+        "sleep:                          \n"
+        "    WFINE                       \n" // if this is not core zero, sleep until interrupted
+        "    BNE   sleep                 \n" // keep sleeping until a handler re-directs this core
+        // initialize memory system
+    );
 }
 
 void undefined_instruction( void ) {
