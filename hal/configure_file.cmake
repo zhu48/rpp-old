be965e5d14fbef6409534ccb58ec389a95cfe41e
Zuodian Hu <huzuodian@gmail.com>
2019-09-28 18:07:50 -0500

    refactor(port): rename RPP_-prefixed variables to HAL_-prefixed

--- hal/configure_file.cmake
+++ hal/configure_file.cmake
@@ -1,13 +1,13 @@
 
 # set required platform-dependent constants
-if(RPP_TARGET_PLATFORM STREQUAL "xc7z")
-    set(RPP_CONTEXT_SIZE "4" CACHE STRING "Size of thread context block")
+if(HAL_TARGET_PLATFORM STREQUAL "xc7z")
+    set(HAL_CONTEXT_SIZE "4" CACHE STRING "Size of thread context block")
 else()
-    set(RPP_CONTEXT_SIZE "8" CACHE STRING "Size of thread context block")
+    set(HAL_CONTEXT_SIZE "8" CACHE STRING "Size of thread context block")
 endif()
 
 # generate header from template
 configure_file(
-	"${RPP_TEMPLATE_DIR}/portable_defs.in"
-	"${RPP_INC_DIR}/portable_defs.hpp"
+	"${HAL_TEMPLATE_DIR}/portable_defs.in"
+	"${HAL_INC_DIR}/portable_defs.hpp"
 )
