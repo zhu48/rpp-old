b7d2e28c3ea2430b24380158027f346e914ea443
Zuodian Hu <huzuodian@gmail.com>
2019-09-30 19:11:00 -0500

    chore(port): put startup functions in anonymous namespace

--- hal/src/arm_cortex_a/startup.cpp
+++ hal/src/arm_cortex_a/startup.cpp
@@ -4,75 +4,90 @@
 
 extern int main( int argc, char* argv[] );
 
-extern "C" __attribute__ ((naked)) void reset( void ) {
-    // In a multi-core system, enable non-primary cores to sleep.
-    __asm__ volatile(
-        // disable all interrupts
-        "    CPSID if                    \n" // disable all IRQ and FIQ interrupts
-        // if this is not core zero, sleep until an interrupt handler makes this core do something
-        "    MRC   p15, 0, R0, c0, c0, 5 \n" // read the Multiprocessor Affinity Register into R0
-        "    ANDS  R0, R0, #3            \n" // mask lowest two bits, which contain the core ID
-        "sleep:                          \n"
-        "    WFINE                       \n" // if this is not core zero, sleep until interrupted
-        "    BNE   sleep                 \n" // keep sleeping until a handler re-directs this core
-    );
+namespace {
 
-    // Initialize the memory system, including the MMU.
-    L1C_DisableCaches();
-    L1C_InvalidateBTAC();
-    L1C_InvalidateICacheAll();
-    L1C_CleanInvalidateDCacheAll();
-    MMU_Disable();
-    MMU_InvalidateTLB();
+    void _start( void ) {
+        main( 0, NULL );
+    }
 
-    // Initialize core mode stacks and registers.
-    // Initialize any critical I/O devices.
-    // Perform any necessary initialization of NEON or VFP.
-    __FPU_Enable();
+    extern "C" __attribute__ ((naked))
+    void reset( void ) {
+        // In a multi-core system, enable non-primary cores to sleep.
+        __asm__ volatile(
+            // disable all interrupts
+            "    CPSID if                    \n" // disable all IRQ and FIQ interrupts
+            // if this is not core zero, sleep until an interrupt makes this core do something
+            "    MRC   p15, 0, R0, c0, c0, 5 \n" // read Multiprocessor Affinity Register into R0
+            "    ANDS  R0, R0, #3            \n" // mask lowest two bits, which contain the core ID
+            "sleep:                          \n"
+            "    WFINE                       \n" // if not core zero, sleep until interrupted
+            "    BNE   sleep                 \n" // stay asleep until a handler re-directs the core
+        );
 
-    // Enable interrupts.
-    // Change core mode or state.
-    // Handle any set-up required for the Secure world (see Chapter 21).
-    // Call the main() application.
-}
+        // Initialize the memory system, including the MMU.
+        L1C_DisableCaches();
+        L1C_InvalidateBTAC();
+        L1C_InvalidateICacheAll();
+        L1C_CleanInvalidateDCacheAll();
+        MMU_Disable();
+        MMU_InvalidateTLB();
 
-extern "C" __attribute__ ((interrupt)) void undefined_instruction( void ) {
+        // Initialize core mode stacks and registers.
+        // Initialize any critical I/O devices.
 
-}
+        // Perform any necessary initialization of NEON or VFP.
+        __FPU_Enable();
 
-extern "C" __attribute__ ((interrupt)) void supervisor_call( void ) {
-    
-}
+        // Enable interrupts.
+        // Change core mode or state.
+        // Handle any set-up required for the Secure world.
 
-extern "C" __attribute__ ((interrupt)) void prefetch_abort( void ) {
-    
-}
+        // Call the main() application.
+        _start();
+    }
 
-extern "C" __attribute__ ((interrupt)) void data_abort( void ) {
-    
-}
+    extern "C" __attribute__ ((interrupt))
+    void undefined_instruction( void ) {
+        
+    }
 
-extern "C" __attribute__ ((interrupt)) void interrupt_request( void ) {
-    
-}
+    extern "C" __attribute__ ((interrupt))
+    void supervisor_call( void ) {
+        
+    }
 
-extern "C" __attribute__ ((interrupt)) void fast_interrupt_request( void ) {
-    
-}
+    extern "C" __attribute__ ((interrupt))
+    void prefetch_abort( void ) {
+        
+    }
 
-extern "C" __attribute__ ((naked,__section__(".reset_table"))) void exception_vector_table( void ) {
-    __asm__ volatile(
-        "B   reset                  \n" // branch to reset handler
-        "B   undefined_instruction  \n" // branch to undefined instruction trap handler
-        "B   supervisor_call        \n" // branch to supervisor call handler
-        "B   prefetch_abort         \n" // branch to prefetch abort handler
-        "B   data_abort             \n" // branch to data abort handler
-        "NOP                        \n"
-        "B   interrupt_request      \n" // branch to IRQ handler
-        "B   fast_interrupt_request \n" // branch to FIQ handler
-    );
-}
+    extern "C" __attribute__ ((interrupt))
+    void data_abort( void ) {
+        
+    }
 
-void _start( void ) {
-    main( 0, NULL );
+    extern "C" __attribute__ ((interrupt))
+    void interrupt_request( void ) {
+        
+    }
+
+    extern "C" __attribute__ ((interrupt))
+    void fast_interrupt_request( void ) {
+        
+    }
+
+    extern "C" __attribute__ ((naked,__section__(".reset_table")))
+    void exception_vector_table( void ) {
+        __asm__ volatile(
+            "B   reset                  \n" // branch to reset handler
+            "B   undefined_instruction  \n" // branch to undefined instruction trap handler
+            "B   supervisor_call        \n" // branch to supervisor call handler
+            "B   prefetch_abort         \n" // branch to prefetch abort handler
+            "B   data_abort             \n" // branch to data abort handler
+            "NOP                        \n"
+            "B   interrupt_request      \n" // branch to IRQ handler
+            "B   fast_interrupt_request \n" // branch to FIQ handler
+        );
+    }
+
 }
