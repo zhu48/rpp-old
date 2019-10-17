3689fdc03bb2f1a4e76eda11f4f2768815c643ba
Zuodian Hu <huzuodian@gmail.com>
2019-10-17 05:30:13 -0500

    feat(port): add system uptime variable

--- hal/src/arm_cortex_a/systick.cpp
+++ hal/src/arm_cortex_a/systick.cpp
@@ -1,8 +1,28 @@
+#include <atomic>
+
 #include "systick.hpp"
 
 using namespace std::chrono_literals;
 
 using namespace portable::systick;
+
+namespace {
+
+    using us_rep_t = std::chrono::microseconds::rep;
+
+    std::atomic<us_rep_t> system_uptime = 0; //!< Global system uptime.
+    us_rep_t              tick_period   = 0; //!< Global system tick period.
+
+    portable::handler_fn_t user_handler = nullptr; //!< User-supplied tick handler.
+
+    void update_tick() {
+        system_uptime += tick_period;
+        if (user_handler) {
+            user_handler();
+        }
+    }
+
+}
 
 std::chrono::microseconds init(
     std::chrono::microseconds period,
@@ -12,5 +32,5 @@ std::chrono::microseconds init(
 }
 
 std::chrono::microseconds uptime() {
-    return 0us;
+    return std::chrono::microseconds{ system_uptime };
 }
